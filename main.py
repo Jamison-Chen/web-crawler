38b844410a7af89921ae29b49277e1df56b4da33
Jamison-Chen <jamison.chen@pinkoi.com>
2024-09-21 00:21:10 +0800

    some minor changes

--- main.py
+++ main.py
@@ -2,39 +2,33 @@ import json
 
 import requests
 from flask import Flask, Response
-from flask import request as flaskRq
-from pyquery import PyQuery as pq
+from flask import request as flask_request
+from pyquery import PyQuery
 
 app = Flask(__name__)
-
-
-def fetchWiki(url):
-    resp = requests.get(url)
-    doc = pq(resp.text)
-    return json.dumps({"body": doc.find("body").html()})
 
 
 @app.route("/")
 def index():
     try:
-        resp = Response(
+        return Response(
             response={"Hello"}, headers={"Access-Control-Allow-Origin": "*"}
         )
     except Exception:
-        resp = "Failed To Fetch"
-    return resp
+        return "Failed"
 
 
 @app.route("/fetchContent", methods=["GET"])
 def fetchContent():
-    url = str(flaskRq.args.get("url"))
     try:
-        resp = Response(
-            response=fetchWiki(url), headers={"Access-Control-Allow-Origin": "*"}
+        url = str(flask_request.args.get("url"))
+        response = PyQuery(requests.get(url).text)
+        return Response(
+            response=json.dumps({"body": response.find("body").html()}),
+            headers={"Access-Control-Allow-Origin": "*"},
         )
     except Exception:
-        resp = "Failed To Fetch"
-    return resp
+        return "Failed To Fetch"
 
 
 if __name__ == "__main__":
